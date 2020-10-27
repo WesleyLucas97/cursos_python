3477e537a6f158c5c9315808d3176918bab49e86
Wesley Lucas <w3sl3ylucas@gmail.com>
2020-10-27 08:32:27 -0300

    aula

---
+++ guppe/aulas/secao_9/list_comprehension.py
@@ -0,0 +1,9 @@
+"""
+List Comprehension
+
+ - Utilizando List Comprehension nós podemos gerar novas listas com dados processados a partir de outro iterável.
+
+# Sintaxe da List Comprehension
+
+
+"""
