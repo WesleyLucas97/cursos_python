65779b37d2eb431a9b5827d7a6b46d4058894c3e
Wesley Lucas <w3sl3ylucas@gmail.com>
2020-10-31 18:28:37 -0300

    aula

--- guppe/aulas/secao_9/list_comprehension_p2.py
+++ guppe/aulas/secao_9/list_comprehension_p2.py
@@ -25,3 +25,7 @@ impares = [numero for numero in numeros if numero % 2]
 print(pares)
 print(impares)
 
+# Exemplo 2
+
+res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
+print(res)
