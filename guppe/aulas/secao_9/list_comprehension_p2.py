2c2e287135256d7030c8cb3576c6963bef627806
Wesley Lucas <w3sl3ylucas@gmail.com>
2021-01-07 18:24:50 -0300

    Aulas

--- guppe/aulas/secao_9/list_comprehension_p2.py
+++ guppe/aulas/secao_9/list_comprehension_p2.py
@@ -18,14 +18,16 @@ print(impares)
 
 # Refatorar
 
-# Qualquer número par módulo de 2 é 0 e 0 em Python é False.
-pares = [numero for numero in numeros if not (numero % 2)]
+# Qualquer número par módulo de 2 é 0 e 0 em Python é False. not False = True
+pares = [numero for numero in numeros if not numero % 2]
+
+# Qualquer número ímpar módulo de 2 é 1, e 1 em Python é True
 impares = [numero for numero in numeros if numero % 2]
 
 print(pares)
 print(impares)
 
-# Exemplo 2
+# 2
 
 res = [numero * 2 if numero % 2 == 0 else numero / 2 for numero in numeros]
 print(res)
---
+++ guppe/aulas/secao_9/listas_aninhadas.py
@@ -0,0 +1,8 @@
+"""
+Listas Aninhadas
+
+    - Algumas linguagens de programação possuem uma estrutura de dados chamadas de arrays:
+        - Unidimensionais (Arrays/Vetores);
+        - Multidimensionais (Matrizes);
+        
+"""
