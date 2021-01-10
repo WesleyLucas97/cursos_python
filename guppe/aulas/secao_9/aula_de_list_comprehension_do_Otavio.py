693de1352bd639e1d252f58644b7373f9ba5a413
Wesley Lucas <w3sl3ylucas@gmail.com>
2021-01-10 01:00:28 -0300

    aula

---
+++ guppe/aulas/secao_9/aula_de_list_comprehension_do_Otavio.py
@@ -0,0 +1,17 @@
+numbers = [1, 2, 3, 4, 5]
+new_numbers = [number for number in numbers]
+division = [number / 2 for number in numbers]
+multiplication = [number * 2 for number in numbers]
+exponentiation = [number ** 2 for number in numbers]
+
+# new_numbers = []
+# for number in numbers:
+#     new_numbers.append(numero)
+
+# new_numbers[0] = 20
+print(numbers)
+print(new_numbers)
+print(division)
+print(multiplication)
+print(exponentiation)
+
