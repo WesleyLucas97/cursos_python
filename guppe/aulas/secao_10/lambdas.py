a0de19a787acb9d84bdc0b146a4029aed72cbceb
Wesley Lucas <w3sl3ylucas@gmail.com>
2021-01-25 23:29:41 -0300

    aula

--- guppe/aulas/secao_10/lambdas.py
+++ guppe/aulas/secao_10/lambdas.py
@@ -21,8 +21,60 @@ calc = lambda x: 3 * x + 1
 print(calc(4))
 print(calc(7))
 
-"""
 
 # Podemos ter expressões lambdas com múltiplas entradas
 
-nome_completo = lambda nomes, sobrenome: nome.strip().title() + ' ' + sobrenome.strip().title()
+nome_completo = lambda nome, sobrenome: nome.strip().title() + ' ' + sobrenome.strip().title()
+
+print(nome_completo(' angeline', 'JOLIE'))
+print(nome_completo('    FEliCity        ', ' jones'))
+
+
+# Em funções Python podemos ter nenhuma ou várias entradas. Em Lambdas também
+
+amar = lambda: 'Como não amar Python? '
+
+uma = lambda x: 3 * x + 1
+
+duas = lambda x, y: (x * y) ** 0.5
+
+tres = lambda x, y, z: 3 / (1 / x + 1/ y + 1 / z)
+
+# n = lambda x1, x2, ..., xn <expressão>
+
+print(amar())
+print(uma(6))
+print(duas(5, 7))
+print(tres(3, 6, 9))
+
+# OBS: Se passarmos mais argumentos do que parâmetros esperados teremos TypeError
+
+
+# Outro exemplo
+
+autores = ['Isaac Asimov', 'Ray Bradbury', 'Robert Heinlein', 'Arthur C. Clarke', 'Olavo de Carvalho', 'J. K. Rollins',
+           'Douglas Adams', 'H. G. Wells', 'Leigh Brackett']
+
+print(autores)
+
+autores.sort(key=lambda sobrenome: sobrenome.split(' ')[-1].lower())
+
+print(autores)
+"""
+
+# Função Quadrática - f(x) = a * (x ** 2) + b * x + c
+
+# Definindo a função
+
+def geradora_funcao_quadratica(a, b, c):
+    """Retorna a função f(x) = a*x**2 + b*x + c"""
+    return lambda x: a * (x ** 2) + b * x + c
+
+
+teste = geradora_funcao_quadratica(2, 3, -5)
+
+print(teste(0))
+print(teste(1))
+print(teste(2))
+
+print(geradora_funcao_quadratica(3, 0, 1)(2))
